84ebff7195adc62b38a6542d7b56cf2923b7e0c5
Messichiquitto <163443627+Messichiquitto@users.noreply.github.com>
2024-10-25 22:57:35 -0300

    Update Main.java

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+//Cambio cambioso
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
