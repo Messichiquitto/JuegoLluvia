78ffcad7dbe7d455643485e63a71c45fa37fefe2
Messichiquitto <163443627+Messichiquitto@users.noreply.github.com>
2024-10-29 20:49:23 -0300

    Arreglo

--- core/src/main/java/io/github/some_example_name/AlienZipZip.java
+++ core/src/main/java/io/github/some_example_name/AlienZipZip.java
@@ -2,12 +2,16 @@ package io.github.some_example_name;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
 public class AlienZipZip extends Personajes implements Interactuable {
 	
+	private Sound zipzip;
+	
 	public AlienZipZip(float x, float y, float ancho, float alto) {
 		super(x, y, ancho, alto, new Texture(Gdx.files.internal("alienZipZip.png")));
+		zipzip = Gdx.audio.newSound(Gdx.files.internal("alien.mp3"));
 	}
 	
 	@Override
